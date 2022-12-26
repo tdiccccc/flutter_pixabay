ff6e4d4176223acb29a3b4f048db400252e029ba
tdiccccc <the.earth3121@gmail.com>
2022-12-26 23:45:41 +0900

    [Add]path_providerとshare_plusを使ってみた

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:share_plus/share_plus.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,9 +32,9 @@ class _PixabayPageState extends State<PixabayPage> {
   //初期値は空
   List hits = [];
 
-  void fetchImages() async {
+  void fetchImages(String text) async {
     Response response = await Dio().get(
-      'https://pixabay.com/api/?key=27974076-f6e3e3dcff4b569e37badd9b6&q=yellow+flowers&image_type=photo&pretty=true'
+      'https://pixabay.com/api/?key=27974076-f6e3e3dcff4b569e37badd9b6&q=$text&image_type=photo&pretty=true&per_page=100'
       );
     hits = response.data['hits'];
     setState(() {
@@ -42,18 +46,71 @@ class _PixabayPageState extends State<PixabayPage> {
   void initState() {
     super.initState();
     //最初に一度だけ呼ばれる。
-    fetchImages();
+    fetchImages('亀');
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: TextFormField(
+          initialValue: '亀',
+          decoration: const InputDecoration(
+            fillColor: Colors.white,
+            filled:true,
+          ),
+          onFieldSubmitted: (text) {
+            fetchImages(text);
+          },
+        ),
+      ),
       body: GridView.builder(
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
         itemCount: hits.length,
         itemBuilder: (context, index){
           Map<String, dynamic> hit = hits[index];
-          return Image.network(hit['previewURL']);
+          return InkWell(
+            onTap: () async {
+              //1. URLから画像をダウンロード
+              Response response = await Dio().get(
+                hit['webformatURL'],
+                options: Options(responseType: ResponseType.bytes),
+              );
+
+              //2. ダウンロードしたデータをファイルに保存
+              Directory dir = await getTemporaryDirectory();
+              File file = await File('${dir.path}/image.png').writeAsBytes(response.data);
+
+              //3. Shareパッケージを呼び出して共有
+              Share.shareFiles([file.path]);
+              // Share.shareFiles([file.path]);
+            },
+            child: Stack(
+              fit: StackFit.expand,
+              children: [
+                Image.network(
+                  hit['previewURL'],
+                  fit: BoxFit.cover,
+                ),
+                Align(
+                  alignment: Alignment.bottomRight,
+                  child: Container(
+                    color: Colors.white,
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        const Icon(
+                          Icons.thumb_up_alt_outlined,
+                          size: 14,
+                        ),
+                        Text('${hit['likes']}'),
+                      ],
+                    )
+                  ),
+                ),
+              ],
+            ),
+          );
         },
       ),
     );
